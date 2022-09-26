526ebb5dff47552e076b78aed0bf4a8f37669b56
Максим Кротков <stalker-2204@mail.ru>
2022-09-26 21:23:54 +0300

    re

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,18 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace Tamagotchi
 {
     internal class Program
     {
+
+        private static Timer aTimer;
+
+
         static void Main(string[] args)
         {
-
             
+            DialogResult res = MessageBox.Show("Запуск таймера?",
+                                   "Тамагочи", MessageBoxButtons.YesNo,
+                                   MessageBoxIcon.Information,
+                                   MessageBoxDefaultButton.Button1, 0);
+            if(res == DialogResult.Yes)
+            {
+                SetTimer();
+                Console.ReadLine();
+                aTimer.Stop();
+                aTimer.Dispose();
+                
+            }
+            else { 
+                Console.WriteLine("Вы не запустили таймер"); 
+            }
 
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
         }
     }
 }
---
+++ Tamagotchi/ToyArgs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Tamagotchi
+{
+    internal class ToyArgs 
+    {
+
+        string title;
+        string text;
+        int icon;
+        static ToyArgs prevToyArgs = null;
+        static List<string> args = new List<string>() { "Хочу есть" , "Хочу спать", "Я заболел", "Хочу играть" , "Пошли гулять"};
+
+        ToyArgs(string title, string text, int icon)
+        {
+
+            this.title = title;
+            this.text = text;
+            this.icon = icon;
+
+        }
+
+        public static ToyArgs getMessageArgs()
+        {
+
+            ToyArgs toyArgs = null;
+
+            Random rand = new Random();
+            int i = rand.Next(0, 4);
+            switch (i)
+            {
+                case 0: IConvertible = 
+            }
+
+            toyArgs = new ToyArgs("Тамагочи", args[i], )
+        }
+        
+    }
+}
